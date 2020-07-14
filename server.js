b10108ea8cb38de94193b4b667ac9d3db444fb9a
Your Name <siddheshjuhukar@gmail.com>
2020-07-14 10:43:49 -0400

    Set up Mongodb connection

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ const express = require('express')
 const dotenv = require('dotenv')
 const logger = require('./middleware/logger')
 const morgan = require('morgan')
+const connectDB = require('./config/db')
+
+dotenv.config({ path: './config/config.env' })
+
+//Connect to Database
+connectDB()
 
 //Route files
 const bootcamps = require('./routes/bootcamps')
 
-dotenv.config({ path: './config/config.env' })
 const app = express()
 
 //Dev logging middleware
@@ -17,6 +22,12 @@ app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000
 const serverType = process.env.NODE_ENV || 'production'
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Server up and running in ${serverType} on port ${PORT}`)
+})
+
+process.on('unhandledRejection', (error, promise) => {
+    console.log(`Error: ${error.message}`)
+    //Close server and exit process
+    server.close(() => process.exit(1))
 })
