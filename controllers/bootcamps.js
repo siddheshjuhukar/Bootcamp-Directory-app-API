206257c76681e7c110d091fa8031d4342820953d
Your Name <siddheshjuhukar@gmail.com>
2020-07-22 19:54:51 -0400

    Added express middleware for error handling and async/await. Added Mongoose middleware and Geocoder

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,85 +1,145 @@
 const Bootcamp = require('../models/Bootcamp')
+const asyncHandler = require('../middleware/async')
+const ErrorResponse = require('../utils/errorResponse')
+const geocoder = require('../utils/geocoder')
 
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  public
-exports.getBootcamps = async (req, res, next) => {
-    try {
-        const bootcamps = await Bootcamp.find()
-        res.status(200).json({success: true, count: bootcamps.length, data: bootcamps})
-    } catch (error) {
-        res.status(400).send({success: false})
+exports.getBootcamps = asyncHandler(async (req, res, next) => {
+    let query
+
+    //Create req query
+    let reqQuery = { ...req.query }
+
+    //Fields to exclude
+    const removeFields = ['select', 'sort', 'page', 'limit']
+
+    //Loop over removeFields and delete them from reqQuery
+    removeFields.forEach(params => delete reqQuery[params])
+
+    console.log(reqQuery)
+
+    //Create query string
+    let queryStr = JSON.stringify(req.query)
+
+    //Create operators ($gt, $gte etc)
+    queryStr = queryStr.replace(/\b(gt|gte|le|lte|in)\b/g, match => `$${match}`)
+
+    query = Bootcamp.find(JSON.parse(queryStr))
+
+    if(req.query.select) {
+        const fields = req.query.select.split(',').join(' ')
+        query = query.select(fields)
     }
-}
+
+    if (req.query.sort) {
+        const sortBy = req.query.sort.split(',').join(' ')
+        query = query.sort(sortBy)
+        console.log(sortBy)
+    } else {
+        query = query.sort('-createdAt')
+    }
+
+    //Pagination
+    const page = parseInt(req.query.page, 10) || 1
+    const limit = parseInt(req.query.limit, 10) || 100
+    const skip = (page - 1) * limit
+
+    query = query.skip(skip).limit(limit)
+
+    const bootcamps = await query
+
+    res.status(200).json({success: true, count: bootcamps.length, data: bootcamps})
+})
 
 // @desc    Get single bootcamp
 // @route   GET /api/v1/bootcamps/:id
 // @access  public
-exports.getBootcamp = async (req, res, next) => {
-    try {
+exports.getBootcamp = asyncHandler(async (req, res, next) => {
         const bootcamp = await Bootcamp.findById(req.params.id)
 
         if (!bootcamp) {
-            return res.status(404).json({success: false})
+            return next(
+                new ErrorResponse(`Bootcamp not found with the id of ${req.params.id}`, 404)
+            )
         }
 
         res.status(200).json({success: true, data: bootcamp})
-    } catch (error) {
-        // res.status(400).json({success: false})
-        next(error)
-    }
-}
+    
+})
 
 // @desc    Create new bootcamp
 // @route   POST /api/v1/bootcamps
 // @access  private
-exports.createBootcamp = async (req, res, next) => {
-    try {
+exports.createBootcamp = asyncHandler(async (req, res, next) => {
         const bootcamp = await Bootcamp.create(req.body)
 
         res.status(201).json({
             success: true,
             data: bootcamp
         })
-    } catch (error) {
-        res.status(400).json({success: false})
-    }
     
-}
+})
 
 // @desc    Update bootcamp
 // @route   PUT /api/v1/bootcamps/:id
 // @access  private
-exports.updateBootcamp = async (req, res, next) => {
-    try {
+exports.updateBootcamp = asyncHandler(async (req, res, next) => {
         const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
             new: true,
             runValidators: true
         })
     
         if(!bootcamp) {
-            return res.status(400).json({success: false})
+            return next(
+                new ErrorResponse(`Bootcamp not found with the id of ${req.params.id}`, 404)
+            )
         }
     
         res.status(200).json({success: true, data: bootcamp})
-    } catch (error) {
-        res.status(400).json({success: false})
-    }
-}
+    
+})
 
 // @desc    Delete bootcamp
 // @route   DELETE /api/v1/bootcamps/:id
 // @access  private
-exports.deleteBootcamp = async (req, res, next) => {
-    try {
+exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
         const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id)
     
         if(!bootcamp) {
-            return res.status(400).json({success: false})
+            return next(
+                new ErrorResponse(`Bootcamp not found with the id of ${req.params.id}`, 404)
+            )
         }
     
         res.status(200).json({success: true, data: {}})
-    } catch (error) {
-        res.status(400).json({success: false})
-    }
-}
+
+})
+
+// @desc    Get bootcamps within a radius
+// @route   GET /api/v1/bootcamps/radius/:zipcode/:distance
+// @access  private
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+    const { zipcode, distance } = req.params
+
+    //Get lat/lng from geocoder
+    const loc = await geocoder.geocode(zipcode)
+    const lat = loc[0].latitude
+    const lng = loc[0].longitude
+
+    //Calc radius using radius
+    //Divide dist by radius of earth
+    //Earth radius = 3963 mi / 6738 km
+    const radius = distance / 3963
+
+    const bootcamps = await Bootcamp.find({
+        location: { $geoWithin: { $centerSphere: [ [ lng, lat ], radius ] } }
+    })
+
+    res.status(200).json({
+        success: true,
+        count: bootcamps.length,
+        data: bootcamps
+    })
+})
--- middleware/error.js
+++ middleware/error.js
@@ -1,10 +1,34 @@
+const ErrorResponse = require('../utils/errorResponse')
+
 const errorHandler = (err, req, res, next) => {
-    //Log to the dev
+    let error = {...err}
+
+    error.message = err.message
+
+    // Log to the dev
     console.log(err)
 
-    res.status(500).json({
+    // Mongoose Bad ObjectId
+    if(err.name === 'CastError') {
+        const message = `Resource not found with id of ${err.value}`
+        error = new ErrorResponse(message, 404)
+    }
+
+    // Mongoose duplicate key
+    if(err.code === 11000) {
+        const message = 'Duplicate field value entered'
+        error = new ErrorResponse(message, 400)
+    }
+
+    // Mongoose validation error 
+    if(err.code === 'ValidationError') {
+        const message = Object.values(err.errors).map(val => val.message)
+        error = new ErrorResponse(message, 400)
+    }
+
+    res.status(error.statusCode || 500).json({
         success: false,
-        error: err.message
+        error: error.message || 'Server Error'
     })
 }
 
